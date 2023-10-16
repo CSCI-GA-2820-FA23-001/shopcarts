3158ff4b4d2b66191849762f31f7c454960302e5
RunbangZhang <rz2473@nyu.edu>
2023-10-16 13:04:13 -0400

    add read-shopcart function

--- service/routes.py
+++ service/routes.py
@@ -2,10 +2,12 @@
 My Service
 
 Describe what your service does here
+GET /shopcarts/{id} - Returns the Shopcart with a given id number
 """
 
 from flask import jsonify, request, url_for, abort
 from service.common import status  # HTTP Status Codes
+from service.models import Shopcart
 
 # Import Flask application
 from . import app
@@ -27,4 +29,24 @@ def index():
 #  R E S T   A P I   E N D P O I N T S
 ######################################################################
 
-# Place your REST API code here ...
+
+######################################################################
+# RETRIEVE A SHOPCART
+######################################################################
+@app.route("/shopcarts/<int:shopcart_id>", methods=["GET"])
+def get_shopcarts(shopcart_id):
+    """
+    Retrieve a single Shopcart
+
+    This endpoint will return a Shopcart based on it's id
+    """
+    app.logger.info("Request for shopcart with id: %s", shopcart_id)
+    shopcart = Shopcart.find(shopcart_id)
+    if not shopcart:
+        abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Shopcart with id '{shopcart_id}' was not found.",
+        )
+
+    app.logger.info("Returning shopcart: %s", shopcart.name)
+    return jsonify(shopcart.serialize()), status.HTTP_200_OK
