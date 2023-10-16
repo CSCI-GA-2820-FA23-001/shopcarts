fe28a7c9bbeabb58fde0eb5707cedaa83b35e328
RunbangZhang <rz2473@nyu.edu>
2023-10-16 13:30:17 -0400

    config test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -9,37 +9,49 @@ import os
 import logging
 from unittest import TestCase
 from service import app
-from service.models import db
+from service.models import db, init_db, Shopcart
 from service.common import status  # HTTP Status Codes
+
+
+DATABASE_URI = os.getenv(
+    "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
+)
+BASE_URL = "/shopcarts"
 
 
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
 # pylint: disable=too-many-public-methods
-class TestYourResourceServer(TestCase):
-    """ REST API Server Tests """
+class TestShopcartServer(TestCase):
+    """REST API Server Tests"""
 
     @classmethod
     def setUpClass(cls):
-        """ This runs once before the entire test suite """
+        """This runs once before the entire test suite"""
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = False
+        # Set up the test database
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+        app.logger.setLevel(logging.CRITICAL)
+        init_db(app)
 
     @classmethod
     def tearDownClass(cls):
-        """ This runs once after the entire test suite """
+        """This runs once after the entire test suite"""
 
     def setUp(self):
-        """ This runs before each test """
+        """This runs before each test"""
         self.client = app.test_client()
 
     def tearDown(self):
-        """ This runs after each test """
+        """This runs after each test"""
 
     ######################################################################
     #  P L A C E   T E S T   C A S E S   H E R E
     ######################################################################
 
     def test_index(self):
-        """ It should call the home page """
+        """It should call the home page"""
         resp = self.client.get("/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
