79ce81bd9261ed37333635a0c42a17cb8b65dbe2
JainiPriya <lj2330@nyu.edu>
2023-10-16 13:03:12 -0400

    Create shopcart

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,5 +1,5 @@
 """
-TestYourResourceModel API Service Test Suite
+TestShopCart API Service Test Suite
 
 Test cases can be run with the following:
   nosetests -v --with-spec --spec-color
@@ -9,37 +9,79 @@ import os
 import logging
 from unittest import TestCase
 from service import app
-from service.models import db
+from service.models import db, Shopcart, init_db
 from service.common import status  # HTTP Status Codes
+from tests.factories import ShopcartFactory
+
+DATABASE_URI = os.getenv(
+    "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
+)
+
+BASE_URL = "/shopcarts"
 
 
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
 # pylint: disable=too-many-public-methods
-class TestYourResourceServer(TestCase):
-    """ REST API Server Tests """
+class TestShopcartServer(TestCase):
+    """Shopcart Server Tests"""
 
     @classmethod
     def setUpClass(cls):
-        """ This runs once before the entire test suite """
+        """This runs once before the entire test suite"""
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+        app.logger.setLevel(logging.CRITICAL)
+        init_db(app)
 
     @classmethod
     def tearDownClass(cls):
-        """ This runs once after the entire test suite """
+        """This runs once after the entire test suite"""
 
     def setUp(self):
-        """ This runs before each test """
+        """This runs before each test"""
         self.client = app.test_client()
+        db.session.query(Shopcart).delete()  # clean up the last tests
+        db.session.commit()
 
     def tearDown(self):
-        """ This runs after each test """
+        """This runs after each test"""
+        db.session.remove()
 
     ######################################################################
-    #  P L A C E   T E S T   C A S E S   H E R E
+    #  S H O P C A R T  T E S T   C A S E S   H E R E
     ######################################################################
 
     def test_index(self):
-        """ It should call the home page """
+        """It should call the home page"""
         resp = self.client.get("/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+    def test_create_shopcart(self):
+        """It should Create a new Shopcart"""
+        shopcart = ShopcartFactory()
+        resp = self.client.post(
+            BASE_URL, json=shopcart.serialize(), content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        # Make sure location header is set
+        location = resp.headers.get("Location", None)
+        self.assertIsNotNone(location)
+
+        # Check the data is correct
+        new_shopcart = resp.get_json()
+        print(new_shopcart)
+        self.assertEqual(
+            new_shopcart["customer_id"],
+            shopcart.customer_id,
+            "Customer Id does not match",
+        )
+        self.assertEqual(
+            new_shopcart["total_price"],
+            shopcart.total_price,
+            "Total price does not match",
+        )
+        self.assertEqual(new_shopcart["items"], shopcart.items, "Items don't not match")
